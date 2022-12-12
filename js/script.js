6cc14ecdfb7e952675118a74fbcfdd50e4bda0d8
leetongchher <tongchherly1245@gmail.com>
2022-12-12 19:59:43 +0700

    new one

--- js/script.js
+++ js/script.js
@@ -66,3 +66,19 @@ body.addEventListener("click" , e =>{
     }
 });
 // Login animation
+const inputCheckbox = document.forms.signin.remember;
+const inputChecked = document.forms.signin.inputCheckbox;
+
+inputCheckbox.addEventListener("click", handleChangeCheckbox);
+
+function handleChangeCheckbox(e) {
+  e.preventDefault();
+
+  if (e.target.value == "") {
+    inputCheckbox.innerHTML = '<i class="ph-check"></i>';
+    inputChecked.checked = true;
+  } else {
+    inputCheckbox.innerHTML = "";
+    inputChecked.checked = false;
+  }
+}
